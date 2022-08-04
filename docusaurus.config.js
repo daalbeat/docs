f592b4b2aa808a75f60550bbf1c275cb36fd4252
Anthony Lee <antwonlee@gmail.com>
2022-08-04 16:18:06 -0500

    chore: Update footer twitter link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
               },
               {
                 label: "Twitter",
-                href: "https://twitter.com/docusaurus",
+                href: "https://twitter.com/daalbeat",
               },
             ],
           },

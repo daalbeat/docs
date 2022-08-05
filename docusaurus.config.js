f3b4717a5422d3eaa9f7bd52a317e7d27fbcf1c2
Anthony Lee <antwonlee@gmail.com>
2022-08-05 00:04:58 -0500

    chore: Remove edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/daalbeat/docs/",
+          // editUrl: "https://github.com/daalbeat/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
